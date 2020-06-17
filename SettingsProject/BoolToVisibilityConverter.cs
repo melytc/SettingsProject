5c44bc577d8ad0e4b33fd8a4c0ebc6c23ab0b6b0
Drew Noakes <git@drewnoakes.com>
2020-06-17 14:28:24 +1000

    Add BoolToVisibilityConverter

---
+++ SettingsProject/BoolToVisibilityConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SettingsProject
+{
+    internal sealed class BoolToVisibilityConverter : IValueConverter
+    {
+        public static BoolToVisibilityConverter HiddenWhenFalse => new BoolToVisibilityConverter(Visibility.Visible, Visibility.Hidden);
+
+        private readonly Visibility _trueVisibility;
+        private readonly Visibility _falseVisibility;
+
+        private BoolToVisibilityConverter(Visibility trueVisibility, Visibility falseVisibility)
+        {
+            _trueVisibility = trueVisibility;
+            _falseVisibility = falseVisibility;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+            {
+                return b ? _trueVisibility : _falseVisibility;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility v)
+            {
+                if (v == _trueVisibility)
+                {
+                    return true;
+                }
+
+                if (v == _falseVisibility)
+                {
+                    return false;
+                }
+            }
+
+            return value;
+        }
+    }
+}
