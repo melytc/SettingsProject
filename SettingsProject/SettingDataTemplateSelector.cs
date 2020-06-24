0f633d2de15f7f034da01f70fd29173b01e2819b
Drew Noakes <git@drewnoakes.com>
2020-06-24 19:45:18 +1000

    Reinstate display of hyperlinks in settings list

---
+++ SettingsProject/SettingDataTemplateSelector.cs
@@ -0,0 +1,19 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SettingsProject
+{
+    internal sealed class SettingDataTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is LinkAction)
+                return LinkActionTemplate;
+
+            return SettingTemplate;
+        }
+
+        public DataTemplate SettingTemplate { get; set; }
+        public DataTemplate LinkActionTemplate { get; set; }
+    }
+}
