f200a2358aecc67695f2a83eb4e194a154122f9c
Drew Noakes <git@drewnoakes.com>
2020-06-26 14:04:48 +1000

    Move NavigationSection to own file

---
+++ SettingsProject/NavigationSection.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+
+namespace SettingsProject
+{
+    internal readonly struct NavigationSection
+    {
+        public string Page { get; }
+        public string Category { get; }
+
+        public NavigationSection(string page, string category)
+        {
+            Page = page;
+            Category = category;
+        }
+
+        public bool Equals(NavigationSection other) => Page == other.Page && Category == other.Category;
+        public override bool Equals(object? obj) => obj is NavigationSection other && Equals(other);
+        public override int GetHashCode() => unchecked(Page.GetHashCode() * 397) ^ Category.GetHashCode();
+        public static bool operator ==(NavigationSection left, NavigationSection right) => left.Equals(right);
+        public static bool operator !=(NavigationSection left, NavigationSection right) => !left.Equals(right);
+
+        public override string ToString() => $"{Page} | {Category}";
+    }
+}
--- SettingsProject/NavigationTree.xaml.cs
+++ SettingsProject/NavigationTree.xaml.cs
@@ -6,26 +6,6 @@ using System.Windows.Input;
 
 namespace SettingsProject
 {
-    internal readonly struct NavigationSection
-    {
-        public string Page { get; }
-        public string Category { get; }
-
-        public NavigationSection(string page, string category)
-        {
-            Page = page;
-            Category = category;
-        }
-
-        public bool Equals(NavigationSection other) => Page == other.Page && Category == other.Category;
-        public override bool Equals(object? obj) => obj is NavigationSection other && Equals(other);
-        public override int GetHashCode() => unchecked(Page.GetHashCode() * 397) ^ Category.GetHashCode();
-        public static bool operator ==(NavigationSection left, NavigationSection right) => left.Equals(right);
-        public static bool operator !=(NavigationSection left, NavigationSection right) => !left.Equals(right);
-
-        public override string ToString() => $"{Page} | {Category}";
-    }
-
     internal partial class NavigationTree
     {
         public static readonly DependencyProperty PagesProperty = DependencyProperty.Register(
