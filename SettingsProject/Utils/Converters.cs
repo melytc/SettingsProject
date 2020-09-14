444a91aa8274d786b0cdc4da35c06e9e5ce42641
Drew Noakes <git@drewnoakes.com>
2020-09-14 15:00:27 +1000

    LambdaMultiConverter supports null

--- SettingsProject/Utils/Converters.cs
+++ SettingsProject/Utils/Converters.cs
@@ -63,33 +63,46 @@ namespace SettingsProject
             }
         }
 
+#nullable disable
         private sealed class LambdaMultiConverter<TFrom1, TFrom2, TTo> : IMultiValueConverter
         {
             private readonly Func<TFrom1, TFrom2, TTo> _convert;
-            private readonly Func<TTo, (TFrom1, TFrom2)>? _convertBack;
+            private readonly Func<TTo, (TFrom1, TFrom2)> _convertBack;
 
-            public LambdaMultiConverter(Func<TFrom1, TFrom2, TTo> convert, Func<TTo, (TFrom1, TFrom2)>? convertBack = null)
+            public LambdaMultiConverter(Func<TFrom1, TFrom2, TTo> convert, Func<TTo, (TFrom1, TFrom2)> convertBack = null)
             {
                 _convert = convert;
                 _convertBack = convertBack;
             }
 
-            public object? Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+            public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
             {
-                if (values.Length == 2 && values[0] is TFrom1 t1 && values[1] is TFrom2 t2)
+                if (values.Length == 3 && TryConvert(values[0], out TFrom1 t1) && TryConvert(values[1], out TFrom2 t2))
                 {
                     return _convert(t1, t2);
                 }
 
                 return DependencyProperty.UnsetValue;
+
+                static bool TryConvert<T>(object o, out T t)
+                {
+                    if (o is T tt)
+                    {
+                        t = tt;
+                        return true;
+                    }
+
+                    t = default;
+                    return o is null;
+                }
             }
 
-            public object?[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
             {
                 if (_convertBack != null && value is TTo to)
                 {
                     var values = _convertBack(to);
-                    return new object?[] { values.Item1, values.Item2 };
+                    return new object[] { values.Item1, values.Item2 };
                 }
 
                 return new[] { DependencyProperty.UnsetValue, DependencyProperty.UnsetValue };
@@ -99,35 +112,48 @@ namespace SettingsProject
         private sealed class LambdaMultiConverter<TFrom1, TFrom2, TFrom3, TTo> : IMultiValueConverter
         {
             private readonly Func<TFrom1, TFrom2, TFrom3, TTo> _convert;
-            private readonly Func<TTo, (TFrom1, TFrom2, TFrom3)>? _convertBack;
+            private readonly Func<TTo, (TFrom1, TFrom2, TFrom3)> _convertBack;
 
-            public LambdaMultiConverter(Func<TFrom1, TFrom2, TFrom3, TTo> convert, Func<TTo, (TFrom1, TFrom2, TFrom3)>? convertBack = null)
+            public LambdaMultiConverter(Func<TFrom1, TFrom2, TFrom3, TTo> convert, Func<TTo, (TFrom1, TFrom2, TFrom3)> convertBack = null)
             {
                 _convert = convert;
                 _convertBack = convertBack;
             }
 
-            public object? Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+            public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
             {
-                if (values.Length == 3 && values[0] is TFrom1 t1 && values[1] is TFrom2 t2 && values[2] is TFrom3 t3)
+                if (values.Length == 3 && TryConvert(values[0], out TFrom1 t1) && TryConvert(values[1], out TFrom2 t2) && TryConvert(values[2], out TFrom3 t3))
                 {
                     return _convert(t1, t2, t3);
                 }
 
                 return DependencyProperty.UnsetValue;
+
+                static bool TryConvert<T>(object o, out T t)
+                {
+                    if (o is T tt)
+                    {
+                        t = tt;
+                        return true;
+                    }
+
+                    t = default;
+                    return o is null;
+                }
             }
 
-            public object?[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
             {
                 if (_convertBack != null && value is TTo to)
                 {
                     var values = _convertBack(to);
-                    return new object?[] { values.Item1, values.Item2, values.Item3 };
+                    return new object[] { values.Item1, values.Item2, values.Item3 };
                 }
 
                 return new[] { DependencyProperty.UnsetValue, DependencyProperty.UnsetValue, DependencyProperty.UnsetValue };
             }
         }
+#nullable enable
 
         private sealed class SingleValueVisibilityConverter : IValueConverter
         {
