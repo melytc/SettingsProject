b863a4172ec40e227e943fe9d298e6261f2d4bfd
Drew Noakes <git@drewnoakes.com>
2020-09-14 13:14:15 +1000

    Support deconstructing KeyValuePair<,>

---
+++ SettingsProject/Utils/DeconstructionExtensions.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+#nullable enable
+
+namespace System.Collections.Generic
+{
+    internal static class DeconstructionExtensions
+    {
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> pair, out TKey key, out TValue value)
+        {
+            key = pair.Key;
+            value = pair.Value;
+        }
+    }
+}
