f554773d1c1b6e44ef42b114f7c21411102f5ca7
Drew Noakes <git@drewnoakes.com>
2020-09-14 20:04:12 +1000

    Move converters to own files

---
+++ SettingsProject/Utils/Converters.LambdaConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+#nullable enable
+
+namespace SettingsProject
+{
+    internal static partial class Converters
+    {
+        private sealed class LambdaConverter<TFrom, TTo> : IValueConverter
+        {
+            private readonly Func<TFrom, TTo> _convert;
+            private readonly Func<TTo, TFrom>? _convertBack;
+
+            public LambdaConverter(Func<TFrom, TTo> convert, Func<TTo, TFrom>? convertBack = null)
+            {
+                _convert = convert;
+                _convertBack = convertBack;
+            }
+
+            public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value is TFrom from)
+                {
+                    return _convert(from);
+                }
+
+                return value;
+            }
+
+            public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (_convertBack != null && value is TTo to)
+                {
+                    return _convertBack(to);
+                }
+
+                return value;
+            }
+        }
+    }
+}
