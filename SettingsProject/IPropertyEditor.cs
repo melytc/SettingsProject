e3cfedba3b53eeae5790bcf0be32d4eaae1bece7
Drew Noakes <git@drewnoakes.com>
2020-09-28 14:54:38 +1000

    Add TODO

--- SettingsProject/IPropertyEditor.cs
+++ SettingsProject/IPropertyEditor.cs
@@ -8,6 +8,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.VS.Implementation.PropertyPages.D
     internal interface IPropertyEditor
     {
         string TypeName { get; }
+        // TODO allow extenders to omit this, resulting in the default template being applied
         DataTemplate PropertyDataTemplate { get; }
         DataTemplate? UnconfiguredDataTemplate { get; }
         DataTemplate? ConfiguredDataTemplate { get; }
